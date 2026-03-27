08249a29b46ccc56d4cc0e32f2480f75d954e813
Emylin01 <emylin.p.p@gmail.com>
2026-03-27 20:03:55 +1100

    Updated with Express API and different UI

--- 3.2P/server.js
+++ 3.2P/server.js
@@ -5,7 +5,31 @@ app.use(express.static(__dirname+'/public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-var port = process.env.port || 3000;
+var port = process.env.PORT || 3000;
+
+const desserts =[
+     {
+        title: "Chocolate Cake",
+        image: "images/chocolate_cake.jpg",
+        link: "View Info",
+        description: "Rich and moist chocolate cake topped with ganache."
+    },
+    {
+        title: "Strawberry Cheesecake",
+        image: "images/cheesecake.jpg",
+        link: "View Info",
+        description: "Creamy cheesecake with fresh strawberry topping."
+    },
+    {
+        title: "Macarons",
+        image: "images/macaron.jpg",
+        link: "View Info",
+        description: "Light and colorful French almond cookies."
+    }
+];
+app.get('/api/desserts', (req, res) => {
+    res.json({ statusCode: 200, data: desserts });
+});
 
 app.listen(port,()=>{
 console.log("App listening to: "+port)
